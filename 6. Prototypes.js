0357b76fb7d8f0c1573c1310b80f3dccde41f480
Sanya <Sanya@Bork.ru>
2022-03-17 20:11:02 +0300

    VSCode Prettier

--- 6. Prototypes.js
+++ 6. Prototypes.js
@@ -1,13 +1,12 @@
-let animal ={
-    move: true,
+let animal = {
+  move: true,
 };
 
 let cat = Object.create(animal, {
-    fly: {
-        value: false,
-        }
-    }
-);
+  fly: {
+    value: false,
+  },
+});
 
 console.log(cat.move);
 console.log(cat.fly);
