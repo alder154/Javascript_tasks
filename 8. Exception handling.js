1ae1c257175e7ca25c4ee0ce2c515a3eafa60365
Sanya <Sanya@Bork.ru>
2022-03-17 20:24:20 +0300

    VSCode Prettier

--- 8. Exception handling.js
+++ 8. Exception handling.js
@@ -1,16 +1,15 @@
 try {
-    console.log(a);
-    let a = 3;
-  } catch (err) {
-    alert('let перед использованием нужно объявить!');
-  };
-
+  console.log(a);
+  let a = 3;
+} catch (err) {
+  alert("let перед использованием нужно объявить!");
+}
 
 try {
-   let n = 1/0;
-   if (n === Infinity) {
-        throw err;
+  let n = 1 / 0;
+  if (n === Infinity) {
+    throw err;
   }
-} catch (err){
-    alert('на ноль делить нельзя!')
-};
+} catch (err) {
+  alert("на ноль делить нельзя!");
+}
