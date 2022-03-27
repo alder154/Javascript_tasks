2a60abbfed4bb1af44056199129f5ae503773579
Sanya <Sanya@Bork.ru>
2022-03-27 12:56:59 +0300

    properly named

---
+++ unit_11/unit_11_2.js
@@ -0,0 +1,10 @@
+import {a} from "./unit_11_1.js";
+console.log(a)
+
+import {square_root} from './unit_11_1.js';
+console.log(square_root(9))
+
+// also works:
+// import * as digits from "./unit_11_1.js";
+// console.log(digits.a);
+// console.log(digits.square_root(16));
